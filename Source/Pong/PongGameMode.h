6936b6b3084c06d423196d2e982f1c8b2e821cd4
Stephen Josey <StephenLJosey@hotmail.com>
2016-10-19 10:40:44 -0700

    Initial commit

---
+++ Source/Pong/PongGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "PongGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PONG_API APongGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
